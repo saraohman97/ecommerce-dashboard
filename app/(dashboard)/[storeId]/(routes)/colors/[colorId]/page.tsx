343469a24c9f6be638303d3f0097f40db182b308
Sara Öhman <saraohman97@gmail.com>
2024-06-10 08:22:49 +0200

    Entities like store, billboard, category, size and color.

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -0,0 +1,19 @@
+import prismadb from "@/lib/prismadb";
+import { ColorForm } from "./components/color-form";
+
+const ColorPage = async ({ params }: { params: { storeId: string } }) => {
+  const color = await prismadb.color.findUnique({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorForm initialData={color} />
+      </div>
+    </div>
+  );
+};
+
+export default ColorPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+
+  return null;
+}
